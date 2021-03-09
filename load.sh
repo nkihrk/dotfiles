eb36d62b74dc7eedf51b2dd9fe8521a891b222cd
NkiHrk <[id+username]@users.noreply.github.co>
2021-03-09 18:52:14 +0900

    [update] changed a directory name

--- load.sh
+++ load.sh
@@ -17,42 +17,42 @@ echo 'All done!'
 echo 'Installing configs...'
 
 # Load fcitx-mozc configs
-ln -sf ~/dotfiles/.xprofile ~/.xprofile
+ln -sf ~/.dotfiles/.xprofile ~/.xprofile
 
 
 # Git config
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 
 
 ## Alacritty
-ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 
 ### Tmux
 
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
 
 ### Fish
 
 # Load envs
-ln -sf ~/dotfiles/.profile ~/.profile
+ln -sf ~/.dotfiles/.profile ~/.profile
 
 # Fish configs
-ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
-ln -sf ~/dotfiles/fish/fish_variables ~/.config/fish/fish_variables
+ln -sf ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -sf ~/.dotfiles/fish/fish_variables ~/.config/fish/fish_variables
 
 
 ### NeoVim
 
 # Create a directory for neovim configs
 mkdir ~/.config/nvim
-ln -sf ~/dotfiles/neovim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/dotfiles/neovim/init.vim ~/.vimrc
+ln -sf ~/.dotfiles/neovim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/.dotfiles/neovim/init.vim ~/.vimrc
 
 # Create a directory for dein tomls
 mkdir ~/.cache/dein/rc
-ln -sf ~/dotfiles/neovim/rc/dein.toml ~/.cache/dein/rc/dein.toml
-ln -sf ~/dotfiles/neovim/rc/dein_lazy.toml ~/.cache/dein/rc/dein_lazy.toml
+ln -sf ~/.dotfiles/neovim/rc/dein.toml ~/.cache/dein/rc/dein.toml
+ln -sf ~/.dotfiles/neovim/rc/dein_lazy.toml ~/.cache/dein/rc/dein_lazy.toml
 
 echo 'All done!'
