0f9f236a798dcaec35577de3f4c9dad05d0ddee4
NkiHrk <[id+username]@users.noreply.github.co>
2021-03-09 19:16:42 +0900

    [update] fixed

--- install-fish-plugins.sh
+++ install-fish-plugins.sh
@@ -3,21 +3,15 @@ echo 'Installing fish plugins...'
 
 # fisher
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-echo 'Installed fisher!'
 # nvm.fish
 fisher install jorgebucaran/nvm.fish
-echo 'Installed nvm.fish!'
 # z
 fisher install jethrokuan/z
-echo 'Installed z!'
 # fzf
 fisher install jethrokuan/fzf
-echo 'Installed fzf!'
 # fish-bd
 fisher install 0rax/fish-bd
-echo 'Installed fish-bd!'
 # fish-ghq
 fisher install decors/fish-ghq
-echo 'Installed fish-ghq!'
 
 echo 'All done!'
